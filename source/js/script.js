67ced0a8394aabe01b890e316b0466fbd0a6348e
formstio <dante1305@yandex.ru>
2024-08-20 23:27:15 +0300

    task6 - corrected
    Ошибки согласно комментариям исправлены

--- source/js/script.js
+++ source/js/script.js
@@ -9,8 +9,8 @@ const buttonOpenProductBig = document.querySelector('.product__button--big');
 const buttonCloseProduct = document.querySelector('.modal__close');
 const buttonCloseNext = document.querySelector('.modal__continue');
 const buttonProductMedium = document.querySelectorAll('.product__button--medium');
-const openModalProduct = document.querySelector('.modal__hidden');
-const closeModalProduct = document.querySelector('.modal__hidden');
+const openModalProduct = document.querySelector('.modal--hidden');
+const closeModalProduct = document.querySelector('.modal--hidden');
 
 const closeModal = function () {
     closeBurger.classList.remove('header__nav--showed');
@@ -20,29 +20,28 @@ const closeModal = function () {
 buttonOpened.addEventListener('click', function () {
     openBurger.classList.add('header__nav--showed');
     buttonClose.addEventListener('click', closeModal);
-})
+});
 
 
 
 const closeProduct = function () {
-    closeModalProduct.classList.remove('modal__hidden--open');
+    closeModalProduct.classList.remove('modal--hidden--open');
     buttonCloseProduct.removeEventListener('click', closeProduct);
     buttonCloseNext.removeEventListener('click', closeProduct)
-}
+};
 
 buttonOpenProductBig.addEventListener('click', function () {
-    openModalProduct.classList.add('modal__hidden--open');
+    openModalProduct.classList.add('modal--hidden--open');
     buttonCloseProduct.addEventListener('click', closeProduct);
     buttonCloseNext.addEventListener('click', closeProduct);
-})
+});
 
 
 buttonProductMedium.forEach(
-  (buttonProductMedium) => {
+    (buttonProductMedium) => {
     buttonProductMedium.addEventListener('click', () => {
-    openModalProduct.classList.add('modal__hidden--open');
+    openModalProduct.classList.add('modal--hidden--open');
     buttonCloseProduct.addEventListener('click', closeProduct);
     buttonCloseNext.addEventListener('click', closeProduct);
-  })
-}
-);
+    })
+});

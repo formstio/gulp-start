96668b301ab5eac11b3544b60e18e48ce42aa1c2
formstio <dante1305@yandex.ru>
2024-09-23 12:26:10 +0300

    lesson - 14(Fetch) - Done

--- source/js/main.js
+++ source/js/main.js
@@ -1,8 +1,11 @@
-import products from './products.js';
+// import products from './products.js';
 import renderProducts from './renderProducts.js';
+
+import {fetchApi} from './api.js';
 
 
 const productsList = document.querySelector('.best-selling__wrapper');
 const productsTemplate = document.querySelector('#product').content;
 
-renderProducts(products, productsTemplate, productsList, false, 'best-selling__product');
+const parseApi = await fetchApi('https://zsa-studio.ru/catalog.php');
+renderProducts(parseApi, productsTemplate, productsList, false, 'best-selling__product');
